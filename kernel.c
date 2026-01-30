7d3314de50ba8040fa1f1c3c875d65dea8ee394e
VrosDean <deanagoritsas155@gmail.com>
2026-01-30 12:51:55 -0500

    DCON v1.0: Fixed all additions (Smart R, Green Screen, Last Line)

--- kernel.c
+++ kernel.c
@@ -1,4 +1,4 @@
-/* 1. Hardware & Variables */
+/* --- 1. HARDWARE & GLOBAL VARIABLES --- */
 unsigned char inb(unsigned short port) {
     unsigned char result;
     __asm__ volatile("inb %1, %0" : "=a"(result) : "Nd"(port));
@@ -9,6 +9,7 @@ volatile char* vidptr = (volatile char*)0xb8000;
 int cursor = 0;
 char cmd_buffer[81]; 
 int cmd_idx = 0;
+int is_green_screen = 0; /* THE FLAG: Must be at the top! */
 
 unsigned char kbd_map[] = { 
     0, 27, '1','2','3','4','5','6','7','8','9','0','-','=','\b','\t',
@@ -17,7 +18,7 @@ unsigned char kbd_map[] = {
     '\\','Z','X','C','V','B','N','M',',','.','/', 0,'*',0,' ' 
 };
 
-/* 2. Screen Helpers */
+/* --- 2. SCREEN HELPERS --- */
 void print_at(const char* message, int row, int col) {
     int index = (row * 80 + col) * 2;
     for (int i = 0; message[i] != '\0'; i++) {
@@ -29,7 +30,8 @@ void print_at(const char* message, int row, int col) {
 
 void draw_ui() {
     unsigned char colors[] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
-    for (int row = 0; row < 25; row++) {
+    /* Rainbow up to row 23 */
+    for (int row = 0; row < 24; row++) {
         unsigned char bg = colors[row % 13] << 4;
         for (int col = 0; col < 80; col++) {
             int index = (row * 80 + col) * 2;
@@ -37,34 +39,43 @@ void draw_ui() {
             vidptr[index + 1] = bg;
         }
     }
-    print_at("DCON v1.0", 5, 35);
-    print_at("DEAN CONSOLE", 6, 34);
-    
-    /* Command Box: 10 lines, last line (row 23) is black */
+    /* Command Box (Black) */
     for (int row = 14; row < 24; row++) {
-        unsigned char box_color = (row == 23) ? 0x00 : 0x00; /* Both black for now */
         for (int col = 4; col < 76; col++) {
             int index = (row * 80 + col) * 2;
             vidptr[index] = ' ';
             vidptr[index + 1] = 0x00; 
         }
     }
+    /* THE VERY LAST LINE (Black) */
+    for (int col = 0; col < 80; col++) {
+        int index = (24 * 80 + col) * 2;
+        vidptr[index] = ' ';
+        vidptr[index + 1] = 0x00;
+    }
+    print_at("DCON v1.0", 5, 35);
+    print_at("DEAN CONSOLE", 6, 34);
+    is_green_screen = 0;
 }
 
-/* 3. Command Logic */
+/* --- 3. COMMAND BRAIN --- */
 void run_command() {
-    if (cmd_buffer[0] == 'D' && cmd_buffer[1] == 'E' && cmd_buffer[2] == 'A' && cmd_buffer[3] == 'N') {
+    /* Check letters 1 by 1: D-E-A-N */
+    if (cmd_buffer[0] == 'D' && cmd_buffer[1] == 'E' && 
+        cmd_buffer[2] == 'A' && cmd_buffer[3] == 'N') {
         for(int i = 0; i < 4000; i += 2) {
             vidptr[i] = ' ';
-            vidptr[i+1] = 0x2F; /* GREEN screen */
+            vidptr[i+1] = 0x2F; /* Green Screen color */
         }
         print_at("DEAN GREEN SCREEN ACTIVATED. PRESS R TO RESET.", 12, 18);
+        is_green_screen = 1;
     }
-    cmd_idx = 0;
+    /* Reset buffer */
     for(int i=0; i<80; i++) cmd_buffer[i] = 0;
+    cmd_idx = 0;
 }
 
-/* 4. Keyboard Driver */
+/* --- 4. KEYBOARD DRIVER --- */
 void check_keyboard() {
     if (inb(0x64) & 0x01) {
         unsigned char scancode = inb(0x60);
@@ -77,19 +88,19 @@ void check_keyboard() {
         } 
         else if (scancode == 0x1C) { /* ENTER */
             run_command();
-            cursor = ((cursor / 80) + 1) * 80 + 5;
+            if (!is_green_screen) cursor = ((cursor / 80) + 1) * 80 + 5;
         }
-        else if (scancode == 0x13) { /* R KEY - Reset Screen */
+        else if (scancode == 0x13 && is_green_screen == 1) { /* SMART R */
             draw_ui();
             cursor = 14 * 80 + 5;
-            cmd_idx = 0;
         }
         else if (scancode < 128) { /* LETTERS */
             char letter = kbd_map[scancode];
             if (letter != 0 && cmd_idx < 79) {
                 cmd_buffer[cmd_idx++] = letter;
                 vidptr[cursor * 2] = letter;
-                vidptr[cursor * 2 + 1] = 0x0F;
+                /* Use green background if in green screen mode */
+                vidptr[cursor * 2 + 1] = (is_green_screen) ? 0x2F : 0x0F;
                 cursor++;
             }
         }
